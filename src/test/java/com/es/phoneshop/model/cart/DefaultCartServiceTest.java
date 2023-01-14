8eac0446e067cfe77243fb9579430ab692e9abef
Daniil Dudak <daniildudak2002@gmail.com>
2023-01-14 16:05:45 +0300

    3.1 task done + done notes from lecture 2

---
+++ src/test/java/com/es/phoneshop/model/cart/DefaultCartServiceTest.java
@@ -0,0 +1,29 @@
+package com.es.phoneshop.model.cart;
+
+import com.es.phoneshop.model.product.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertTrue;
+
+public class DefaultCartServiceTest {
+    private ProductDao productDao;
+    private CartService cartService;
+
+    @Before
+    public void setup() {
+        productDao =  ArrayListProductDao.getInstance();
+        cartService = DefaultCartService.getInstance();
+    }
+
+    @Test
+    public void testFindProductsNoResults() {
+        Product product = new Product();
+        CartItem cartItem = new CartItem(product, 1);
+        productDao.save(product);
+        cartService.add(product.getId(), 1);
+        assertTrue(cartService.getCart().getCartItems().contains(cartItem));
+    }
+}
