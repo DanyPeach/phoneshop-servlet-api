8834ba752501cdf36a4c0dfa956045386f542885
Daniil Dudak <daniildudak2002@gmail.com>
2023-01-15 15:54:29 +0300

    task 3.2

--- src/main/java/com/es/phoneshop/model/cart/CartService.java
+++ src/main/java/com/es/phoneshop/model/cart/CartService.java
@@ -1,6 +1,8 @@
 package com.es.phoneshop.model.cart;
 
+import com.es.phoneshop.exception.OutOfStockException;
+
 public interface CartService {
     Cart getCart();
-    void add(long productId, int Quantity);
+    void add(long productId, int Quantity) throws OutOfStockException;
 }
